04d399f92a13a34b4af50d16a71fe54cdea37e36
ductruung1 <102138981+ductruung1@users.noreply.github.com>
2022-04-12 12:54:09 +0700

    Add files via upload

---
+++ mint-steel-ixora-2022-04-12_055208/app/server.js
@@ -0,0 +1,188 @@
+let Discord = require("discord.js");
+let Client = new Discord.Client();
+
+Client.on("message", (message) => {
+  if (message.content === "Iu em") {
+    message.channel.send("Iu anh <3");
+  }
+  if (message.content === "Nhớ anh hok") {
+    message.channel.send("Dạ nhớ :<");
+  }
+
+  if (message.content === "Hello em") {
+    message.channel.send("Hi anh :3");
+  }
+
+  if (message.content === "Em đang làm gì đấy") {
+    let trl = ["Đang nt với anh nè :3", "Chờ anh nè :>"];
+    message.channel.send(`${trl[Math.floor(Math.random() * trl.length)]}`);
+  }
+
+  if (message.content === "Em ơii") {
+    let trl = ["Dạ", "Em đây ạ :3", "Anh gọi gì đấy", "Ơi em đây :3"];
+    message.channel.send(`${trl[Math.floor(Math.random() * trl.length)]}`);
+  }
+
+  if (message.content === "ỏ :3") {
+    message.channel.send(":3");
+  }
+
+  if (message.content === "Anh Đạt") {
+    let trl = ["Đẹp zai", "Cuto", "pro vjp"];
+    message.channel.send(`${trl[Math.floor(Math.random() * trl.length)]}`);
+  }
+
+  if (message.content === ":c") {
+    message.channel.send("bùn hỏ :<");
+  }
+
+  if (message.content === "thái") {
+    let trl = ["Nghiện hent", "hentai", "lolicon", "Óc bò"];
+    message.channel.send(`${trl[Math.floor(Math.random() * trl.length)]}`);
+  }
+
+  if (message.content === "pew") {
+    message.channel.send("Simp lỏd");
+  }
+
+  if (message.content === "Anh đi ngủ nha") {
+    message.channel.send("bùn ngủ r à :<");
+  }
+
+  if (message.content === "Um ngủ nh :<") {
+    message.channel.send("uci anhiu ngủ ngon :3");
+  }
+
+  if (message.content === "Loli") {
+    message.channel.send(
+      "https://cdn.glitch.me/74320f64-978f-4104-8ddb-2992b4440a41%2Fb6dd7b34-3e7e-47e0-b44e-4b7bb1da279f.image.png?v=1639230049573"
+    );
+  }
+
+  if (message.content === "Bùn wá") {
+    message.channel.send("Vô đây đỡ chán :3 https://www.hentaiz.cc/");
+  }
+
+  if (message.content === "Loli2") {
+    message.channel.send(
+      "https://cdn.glitch.me/74320f64-978f-4104-8ddb-2992b4440a41%2Fh%C3%ACnh%20%E1%BA%A3nh_2021-12-11_204524.png?v=1639230338883"
+    );
+  }
+
+  if (message.content === "Eiu oi dậy chưa nà") {
+    message.channel.send("Dạ em dậy ròi :3");
+  }
+
+  if (message.content === "Iu anh kh") {
+    message.channel.send("Dạ iuu");
+  }
+
+  if (message.content === "Dễ thw ghe") {
+    message.channel.send(":3");
+  }
+
+  if (message.content === "Anh chơi game nha") {
+    message.channel.send("Okii");
+  }
+
+  if (message.content === "đóe") {
+    message.channel.send(">:c");
+  }
+
+  if (message.content === "hmu hmu") {
+    message.channel.send("Anh ơi");
+  }
+
+  if (message.content === "Ơi em") {
+    message.channel.send("Anh có iu em hok :<");
+  }
+
+  if (message.content === "Iu chứ :3") {
+    message.channel.send("ỏ");
+  }
+
+  if (message.content === "._.") {
+    message.channel.send(".__.");
+  }
+
+  if (message.content === ".ping") {
+    message.channel.send("@everyone");
+  }
+
+  if (message.content === "đạt nó ám sát anh :<") {
+    message.channel.send(
+      "Đừng lo kh những em là vợ anh em con là vệ sĩ của anh nữa >:3"
+    );
+  }
+
+  if (message.content === "Ai đẹp trai nhất") {
+    message.channel.send("Anh Hoàngg :>");
+  }
+
+  if (message.content === "sao em dễ thw thế") {
+    message.channel.send("hihi:3");
+  }
+
+  if (message.content === "trẻ trâu") {
+    message.channel.send("m trẻ trâu nín");
+  }
+
+  if (message.content === "FBI") {
+    message.channel.send(
+      "https://cdn.glitch.me/74320f64-978f-4104-8ddb-2992b4440a41%2Fh%C3%ACnh%20%E1%BA%A3nh_2021-12-12_170850.png?v=1639303737839"
+    );
+  }
+
+  if (message.content === "FBI2") {
+    message.channel.send(
+      "https://cdn.glitch.me/74320f64-978f-4104-8ddb-2992b4440a41%2Fh%C3%ACnh%20%E1%BA%A3nh_2021-12-12_170906.png?v=1639303750928"
+    );
+  }
+
+  if (message.content === "Địt nhau hok em?") {
+    let trl = ["đóe", "có cái l nha anh", "cút"];
+    message.channel.send(`${trl[Math.floor(Math.random() * trl.length)]}`);
+  }
+
+  if (message.content === "Địt nhau ko em?") {
+    let trl = ["đóe", "Có cái l nha anh", "cút"];
+    message.channel.send(`${trl[Math.floor(Math.random() * trl.length)]}`);
+  }
+
+  if (message.content === "Địt nhau hok em") {
+    let trl = ["đóe", "Có cái l nha anh", "cút"];
+    message.channel.send(`${trl[Math.floor(Math.random() * trl.length)]}`);
+  }
+
+  if (message.content === "Địt nhau ko em") {
+    let trl = ["đóe", "Có cái l nha anh", "cút"];
+    message.channel.send(`${trl[Math.floor(Math.random() * trl.length)]}`);
+  }
+
+  if (message.content === "Đjt nhau hok em") {
+    let trl = ["đóe", "Có cái l nha anh", "cút"];
+    message.channel.send(`${trl[Math.floor(Math.random() * trl.length)]}`);
+  }
+
+  if (message.content === "xin lũi :<") {
+    message.channel.send(";-;");
+  }
+
+  if (message.content === "Tắm chung với anh hok :>") {
+    message.channel.send("Uci c:");
+  }
+
+  if (message.content === "Nó bắt nạt anh kìa :<") {
+    message.channel.send("Đâu tk nào >:c");
+  }
+
+  if (message.content === "Làm vợ anh nha") {
+    message.channel.send("hong pé ơi");
+  }
+
+  if (message.content === "làm vợ anh nha") {
+    message.channel.send("hong pé ơi");
+  }
+});
+
+ Client.login("OTYxMTUzNzgzNjEyNzAyNzMw.Yk02Nw.yNmUw-G9B9G2Hb2QRLHfOOKH6Ao");
